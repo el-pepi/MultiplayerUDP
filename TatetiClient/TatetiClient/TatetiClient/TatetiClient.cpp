af09a8f3eafaaee7923528cfcbe5a54a94425c08
el-pepi <el-pepi@live.com.ar>
2017-10-11 22:04:24 -0300

    Ya se pueden pasar structs. Falta llegar a estados de movimientos

--- TatetiClient/TatetiClient/TatetiClient/TatetiClient.cpp
+++ TatetiClient/TatetiClient/TatetiClient/TatetiClient.cpp
@@ -8,6 +8,7 @@
 #include<stdio.h>
 #include<winsock2.h>
 #include<iostream>
+#include <stdlib.h>
 using namespace std;
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
@@ -20,19 +21,31 @@ int port = 0;
 SOCKET s;
 int slen;
 
+string name;
+
 enum MessageType
 {
 	Connect,
 	Fail,
 	Success,
 	Name,
-	Move
+	Move,
+	Update
 };
+
+enum GameStates {
+	Connecting,
+	Waiting,
+	MyTurn,
+	NotMyTurn
+};
+
+GameStates gameState;
 
 struct Message {
 public:
 	MessageType type;
-	char* msg;
+	char msg[256];
 };
 
 void SendMsg(sockaddr_in adr, Message msg) {
@@ -52,12 +65,45 @@ void SendMsg(sockaddr_in adr, Message msg) {
 	}
 }
 
+void DrawTable() {
+	
+	system("cls");
+	printf("%s%s\n","You are: ",name.c_str());
+}
+
+void GetData(char buf[BUFLEN]) {
+	Message* m = reinterpret_cast<Message*>(buf);
+	switch (gameState)
+	{
+	case GameStates::Connecting:
+		if (m->type == MessageType::Success) {
+			name = m->msg;
+			system("cls");
+			printf("%s%s\n%s\n", "You are: ", m->msg, "Waiting for other player to connect...");
+			gameState = GameStates::Waiting;
+		}
+		else
+		{
+			printf("%s\n", m->msg);
+			system("pause");
+			exit(0);
+		}
+		break;
+	case GameStates::Waiting:
+		if (m->type == MessageType::Success) {
+			DrawTable();
+			printf("%s\n", m->msg);
+		}
+		break;
+	}
+}
+
 int main()
 {
 	struct sockaddr_in si_other;
 	slen = sizeof(si_other);
 	char buf[BUFLEN];
-	char message[BUFLEN];
+	//char message[BUFLEN];
 	WSADATA wsa;
 
 	//Initialise winsock
@@ -77,7 +123,7 @@ int main()
 	}
 
 	printf("Enter server ip: ");
-	cin >> ip;
+	cin.getline(ip,sizeof(ip));
 
 	printf("Enter server port: ");
 	cin >> port;
@@ -89,24 +135,31 @@ int main()
 	si_other.sin_port = htons(port);
 	si_other.sin_addr.S_un.S_addr = inet_addr(ip);
 
+	Message msg;
+	msg.type = MessageType::Connect;
+	strcpy_s(msg.msg, "Connecting");
+	SendMsg(si_other,msg);
+
+	if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
+	{
+		printf("recvfrom() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	//DrawTable();
+	GetData(buf);
+
 	//start communication
 	while (1)
 	{
-		Message msg;
-		msg.type = MessageType::Move;
-		msg.msg = "Hola";
-
-		SendMsg(si_other,msg);
-		/*
-		printf("Enter message : ");
-		cin >> message;
-		//send the message
-		if (sendto(s, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
-		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
+		if (gameState == GameStates::MyTurn) {
+			Message m;
+			m.type = MessageType::Move;
+			printf("Enter position: ");
+			cin.get();
+			cin.getline(m.msg,sizeof(m.msg));
+			SendMsg(si_other,m);
 		}
-		*/
+
 		//receive a reply and print it
 		//clear the buffer by filling null, it might have previously received data
 		memset(buf, '\0', BUFLEN);
@@ -116,10 +169,9 @@ int main()
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
 			exit(EXIT_FAILURE);
 		}
-
-		Message* message = reinterpret_cast<Message*>(buf);
-		printf(message->msg);
-		break;
+		GetData(buf);
+		//Message* message = reinterpret_cast<Message*>(buf);
+		//printf("%s\n", message->msg);
 	}
 
 	closesocket(s);
--- TatetiServer/TatetiServer/TatetiServer.cpp
+++ TatetiServer/TatetiServer/TatetiServer.cpp
@@ -7,6 +7,7 @@
 #include<stdio.h>
 #include<winsock2.h>
 #include<iostream>
+#include<sstream>
 using namespace std;
 
 #pragma comment(lib,"ws2_32.lib") //Winsock Library
@@ -16,6 +17,9 @@ using namespace std;
 int port = 0;
 SOCKET s;
 int slen;
+
+int values[9] = {0};
+int turn = 0;
 
 class Player {
 public:
@@ -28,27 +32,47 @@ enum MessageType
 	Fail,
 	Success,
 	Name,
-	Move
+	Move,
+	Update
 };
 
 struct Message {
 public:
 	MessageType type;
-	char* msg;
+	char msg[256];
 };
 
-void SendMsg(sockaddr_in adr,Message* msg) {
+string GetTable() {
+	string s = "";
+	for (int i = 0; i < 9; i++)
+	{
+		if (values[i] == 0) {
+			s += i;
+		}if (values[i] == -1) {
+			s += "X";
+		}if (values[i] == 1) {
+			s += "O";
+		}if (i % 3 == 0) {
+			s += "\n";
+		}
+	}
+	return s;
+}
+
+void SendMsg(sockaddr_in adr, Message msg) {
 
 	char m[BUFLEN];
-	memset(m, '\0', BUFLEN);
-	memcpy(m, &msg, sizeof(msg));
-	//memcpy(m + sizeof(msg->type), &msg->msg, sizeof(msg->msg));
-	//memcpy(m, &msg->type, sizeof(msg->type));
 
-	Message* message = reinterpret_cast<Message*>(m);
-	if (sendto(s, m, sizeof(m), 0, (struct sockaddr*) &adr, slen) == SOCKET_ERROR)
+	memcpy(m, &msg, sizeof(Message));
+
+	Message message;
+	memcpy(&message, m, sizeof(Message));
+
+
+	if (sendto(s, m, sizeof(Message), 0, (struct sockaddr*) &adr, slen) == SOCKET_ERROR)
 	{
 		printf("sendto() failed with error code : %d", WSAGetLastError());
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
 }
@@ -71,6 +95,7 @@ int main()
 	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
 	{
 		printf("Failed. Error Code : %d", WSAGetLastError());
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
 	printf("Initialised.\n");
@@ -79,6 +104,8 @@ int main()
 	if ((s = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
 	{
 		printf("Could not create socket : %d", WSAGetLastError());
+		system("pause");
+		exit(EXIT_FAILURE);
 	}
 	printf("Socket created.\n");
 
@@ -91,6 +118,7 @@ int main()
 	if (bind(s, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
 	{
 		printf("Bind failed with error code : %d", WSAGetLastError());
+		system("pause");
 		exit(EXIT_FAILURE);
 	}
 	puts("Bind done");
@@ -115,21 +143,21 @@ int main()
 		if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
 		{
 			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			system("pause");
 			exit(EXIT_FAILURE);
 		}
 
 
 		//print details of the client/peer and the data received
 		printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-		printf("Data: %s\n", buf);
-
 
 		Message message;
 
 		memcpy(&message, buf, recv_len);
-		//Message* message = reinterpret_cast<Message*>(buf);
 
-		Message* m = new Message();
+		printf("Data: %s\n", message.msg);
+
+		Message m;
 		switch (message.type)
 		{
 		case MessageType::Connect:
@@ -137,32 +165,70 @@ int main()
 				Player* p = new Player();
 				p->address = si_other;
 				players[connectedPlayers] = p;
+				m.type = MessageType::Success;
+				if (connectedPlayers == 0) {
+					strcpy_s(m.msg, "X");
+				}
+				else
+				{
+					strcpy_s(m.msg, "O");
+				}
+				SendMsg(p->address,m);
+
 				connectedPlayers++;
-				m->type = MessageType::Success;
-				m->msg = "Conectado exitosamente";
-				SendMsg(si_other,m);
+				if (connectedPlayers == 2) {
+					strcpy_s(m.msg, "Game start!");
+					for (int i = 0; i < 2; i++)
+					{
+						SendMsg(players[i]->address, m);
+					}
+				}
 			}
 			else
 			{
-				m->type = MessageType::Fail;
-				m->msg = "ERROR: Server lleno";
+				m.type = MessageType::Fail;
+				strcpy_s(m.msg, "ERROR: Server lleno");
+				SendMsg(si_other, m);
+			}
+			break;
+		case MessageType::Move:
+			int mov = (int)(message.msg[0] - 48);
+			if (message.msg[1] == '\0' && mov > 0 && mov < 10) {
+				SendMsg(si_other, m);
+				if (values[mov] == 0) {
+					if (turn == 0) {
+						turn = 1;
+						values[mov] = -1;
+					}
+					else {
+						turn = 0;
+						values[mov] = 1;
+					}
+					m.type = MessageType::Update;
+					strcpy_s(m.msg, GetTable().c_str());
+					for (int i = 0; i < 2; i++)
+					{
+						SendMsg(players[i]->address, m);
+					}
+				}
+				else {
+					m.type = MessageType::Fail;
+					strcpy_s(m.msg, "ERROR: Position taken");
+					SendMsg(si_other, m);
+				}
+			}
+			else
+			{
+				m.type = MessageType::Fail;
+				strcpy_s(m.msg, "ERROR: Unvalid position");
 				SendMsg(si_other, m);
 			}
 			break;
 		default:
-			m->type = MessageType::Fail;
-			m->msg = "ERROR: Comando desconocido";
+			m.type = MessageType::Fail;
+			strcpy_s(m.msg, "ERROR: Comando desconocido");
 			SendMsg(si_other, m);
 			break;
-		}
-
-		strlen(buf);
-
-		//now reply the client with the same data
-		if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
-		{
-			printf("sendto() failed with error code : %d", WSAGetLastError());
-			exit(EXIT_FAILURE);
 		}
 	}
 
